d13696d03534d95d619e0536ae1173c0f10efb0b
Alfredo Medina <alfremedpal@gmail.com>
2021-05-09 18:19:04 -0500

    Update home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Heading } from '@chakra-ui/core'
 
 export default function Home() {
 
     return (
         <div>
-            HOME
+            <Heading>
+                Board Game Picker
+            </Heading>
         </div>
     )
 }
